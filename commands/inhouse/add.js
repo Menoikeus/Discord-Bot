8e3892973055767a06375672050fe445dbd87110
Menoikeus <datonnguyen@gmail.com>
2017-12-31 02:51:37 -0500

    Realized difference between var, let, const, and nothing
    -Started changing code from var to const (I've worked on start and other functions but I'm committing this early)
    -Outputs summoner.name instead of just name

--- commands/inhouse/add.js
+++ commands/inhouse/add.js
@@ -6,22 +6,22 @@ var db;
 exports.run = async (client, message, args) => {
   try {
     db = await mongodb.getDb();
-    directoryid = await redirector.getDirectoryId(db, message.member.guild);
+    const directoryid = await redirector.getDirectoryId(db, message.member.guild);
 
-    var name = args.join(" ");
+    const name = args.join(" ");
     console.log("Trying to insert player " + name);
 
     // Get directory and inhouse accounts
-    var user = await db.db(directoryid).collection("users").find({ userid: message.member.user.id }).toArray();
-    var inhouse_user = await db.db(directoryid).collection("inhouse_players").find({ userid: message.member.user.id }).toArray();
+    const user = await db.db(directoryid).collection("users").find({ userid: message.member.user.id }).toArray();
+    const inhouse_user = await db.db(directoryid).collection("inhouse_players").find({ userid: message.member.user.id }).toArray();
     if(inhouse_user.length == 0) {
       try {
-        var summoner = await lolapi.Summoner.gettingByName(name);
+        const summoner = await lolapi.Summoner.gettingByName(name);
 
         // See if this summoner has already been registered
-        var summoner_in_db = await db.db(directoryid).collection("inhouse_players").find({ leagueid: summoner.id }).toArray();
+        const summoner_in_db = await db.db(directoryid).collection("inhouse_players").find({ leagueid: summoner.id }).toArray();
         if(summoner_in_db.length == 0) {
-          var leagues = await lolapi.League.gettingPositionsForSummonerId(summoner.id);
+          const leagues = await lolapi.League.gettingPositionsForSummonerId(summoner.id);
 
           var info;
           var rank = -1;
@@ -58,7 +58,7 @@ exports.run = async (client, message, args) => {
               rank = tempRank > rank ? tempRank : rank;
             }
             info = {
-              "userid"	      : user[0].userid,
+              "userid"	    : user[0].userid,
               "leaguename"  : summoner.name,
               "leagueid"    : summoner.id,
               "elo"         : rank
@@ -66,7 +66,7 @@ exports.run = async (client, message, args) => {
           }
 
           db.db(directoryid).collection("inhouse_players").insertOne(info).then( () => {
-            message.reply("I've successfully added your account " + name + " with rank " + rank);
+            message.reply("I've successfully added your account " + summoner.name + " with rank " + rank);
           });
         }
         else {
@@ -76,11 +76,13 @@ exports.run = async (client, message, args) => {
       catch(err) {
         // See if the error occurred because the summoner doesn't exist
         // Then throw the error
-        if(err.statusCode == 404) {
+        if(err.hasOwnProperty('statusCode') && err.statusCode == 404) {
             console.log("lolapi error " + err.statusCode + ": Summoner not found");
             message.reply("that summoner name does not exist!");
         }
-        throw err;
+        else {
+          console.log(err);
+        }
       }
     }
     else {
