3f1d6ca304dd636e97857f492d02346ae9f2ce53
Menoikeus <datonnguyen@gmail.com>
2017-12-31 18:10:19 -0500

    Worked on game start
    -Game start can now get current game
    -Gets game stats, players, status. Can match player to participant
    -Almost finished with getting the end game procedure to work

--- commands/inhouse/game/start.js
+++ commands/inhouse/game/start.js
@@ -7,27 +7,27 @@ var db;
 exports.run = async (client, message, args) => {
   try {
     db = await mongodb.getDb();
-    directoryid = await redirector.getDirectoryId(db, message.member.guild);
+    const directoryid = await redirector.getDirectoryId(db, message.member.guild);
 
     // See if there's an unfinished game pending
-    unfinished_games = await db.db(directoryid).collection("inhouse_matches").find({ done: false }).toArray();
+    const unfinished_games = await db.db(directoryid).collection("inhouse_matches").find({ done: false }).toArray();
     if(unfinished_games.length == 0) {
 
       // See if the player that called this command is in the inhouse league
-      player = await db.db(directoryid).collection("inhouse_players").find({ userid: message.member.user.id }).toArray();
+      const player = await db.db(directoryid).collection("inhouse_players").find({ userid: message.member.user.id }).toArray();
       if(player.length != 0) {
         try {
           // Get the game the player is currently in
-          game = await lolapi.Spectator.gettingActiveGame(player[0].leagueid);
+          const game = await lolapi.Spectator.gettingActiveGame(player[0].leagueid);
 
           // Check if its a custom game
           if(game.gameType == "CUSTOM_GAME") {
-            inhouse_players = await db.db(directoryid).collection("inhouse_players").find({}).toArray();
+            var inhouse_players = await db.db(directoryid).collection("inhouse_players").find({}).toArray();
 
             // See if we have enough inhouse league players in the game
             var num_players_in_league = 0;
             for(i in game.participants) {
-              id = game.participants[i].summonerId;
+              const id = game.participants[i].summonerId;
               for(j in inhouse_players) {
                 if(id == inhouse_players[j].leagueid) {
                   num_players_in_league++;
@@ -37,10 +37,11 @@ exports.run = async (client, message, args) => {
 
             // If we have enough qualified players
             if(num_players_in_league >= 1) {
-              match_data = {
+              const match_data = {
                 matchid   : game.gameId + "",
                 done      : false
               }
+
 
               await db.db(directoryid).collection("inhouse_matches").insertOne(match_data);
               message.channel.send("A new inhouse match has begun");
@@ -49,29 +50,41 @@ exports.run = async (client, message, args) => {
               var checker = schedule.scheduleJob('*/30 * * * * *', async function() {
                 console.log("CHECKING");
                 try {
-                  finished_game = await lolapi.Match.gettingById(game.gameId);
+                  const finished_game = await lolapi.Match.gettingById(game.gameId);
 
                   var participant_stats = [];
                   var participant_userids = [];
+                  console.log("1");
                   for(i in players_in_league) {
-                    championid = players_in_league[i].championid;
-                    teamid = players_in_league[i].teamid;
+                    console.log("2");
+                    const championid = players_in_league[i].championid;
+                    const teamid = players_in_league[i].teamid;
                     for(j in finished_game.participants) {
+                      console.log("3");
                       if(championid == finished_game.participants[j].championId && teamid == finished_game.participants[j].teamId) {
-                        participant_stats.push({
+                        console.log("4");
+                        await participant_stats.push({
                           userid        : players_in_league[i].userid,
                           championId    : championid,
                           official_data : finished_game.participants[j]
                         });
-                        participant_userids.push(players_in_league[i].userid);
+                        await participant_userids.push(players_in_league[i].userid);
                       }
                     }
                   }
 
-                  winners = finished_game.teams[0].win == "Fail" ? 200 : 100;
+                  console.log(finished_game);
+                  console.log(participant_stats);
+                  console.log(players_in_league);
 
+                  const winners = finished_game.teams[0].win == "Fail" ? 200 : 100;
+                  console.log(await db.db(directoryid).collection("inhouse_matches").find({ matchid: game.gameId }));
+                  console.log("H");
+                  console.log(game.gameId);
+                  console.log(finished_game.gameId);
+                  console.log(game);
                   await db.db(directoryid).collection("inhouse_matches").update(
-                    { matchid: game.gameId },
+                    { matchid: finished_game.gameId },
                     { $set:
                       {
                         players       :  participant_stats,
@@ -80,22 +93,29 @@ exports.run = async (client, message, args) => {
                         done          :  true
                       }
                     });
+                  message.channel.send("Inhouse game completed!");
                   checker.cancel();
                 }
                 catch(err) {
                   // If the game hasn't finished yet
-                  if(err.statusCode == 404) {
+                  if(err.hasOwnProperty('statusCode') && err.statusCode == 404) {
                     try {
-                      ongoing_game = await lolapi.Spectator.gettingActiveGame(player[0].leagueid);
+                      const ongoing_game = await lolapi.Spectator.gettingActiveGame(player[0].leagueid);
+                      console.log(directoryid);
+                      //directoryid = await redirector.getDirectoryId(db, message.member.guild);
+                      const inhouse_players = await db.db(directoryid).collection("inhouse_players").find({}).toArray();
+
+                      console.log(ongoing_game);
+                      console.log(inhouse_players);
 
                       players_in_league = [];
                       // Update current inhouse league players in game
                       // (So that you can !inhouse add while game is ongoing)
                       for(i in ongoing_game.participants) {
-                        id = ongoing_game.participants[i].summonerId;
+                        const id = ongoing_game.participants[i].summonerId;
                         for(j in inhouse_players) {
                           if(id == inhouse_players[j].leagueid) {
-                            players_in_league.push({
+                            await players_in_league.push({
                               userid      : inhouse_players[j].userid,
                               championid  : ongoing_game.participants[i].championId,
                               teamid      : ongoing_game.participants[i].teamId
@@ -105,18 +125,22 @@ exports.run = async (client, message, args) => {
                       }
                       console.log(players_in_league);
                     }
-                    catch(err2) {
+                    catch(err) {
                       // If the game looks like it hasn't finished, AND the summoner is no longer player in a game,
                       // then the game must have ended prematurely. Thus discard the match
-                      if(err.statusCode == 404) {
+                      if(err.hasOwnProperty('statusCode') && err.statusCode == 404) {
                         message.channel.send("Game ended prematurely! Not counting match.");
                         db.db(directoryid).collection("inhouse_matches").remove({ done: false }, true);
                         checker.cancel();
                       }
-                      throw err2;
+                      else {
+                        console.log(err);
+                      }
                     }
                   }
-                  throw err;
+                  else {
+                    console.log(err);
+                  }
                 }
               });
             }
@@ -131,11 +155,13 @@ exports.run = async (client, message, args) => {
         catch(err) {
           // See if the error occurred because the game doesn't exist
           // Then throw the error
-          if(err.statusCode == 404) {
+          if(err.hasOwnProperty('statusCode') && err.statusCode == 404) {
               console.log("lolapi error " + err.statusCode + ": Game not found");
               message.reply("you're not in a game right now!");
           }
-          throw err;
+          else {
+            console.log(err);
+          }
         }
       }
       else {
@@ -148,24 +174,29 @@ exports.run = async (client, message, args) => {
   }
   catch(err) {
     // Error catching for league api
-    switch(err.statusCode) {
-      case 400: console.log("lolapi error " + err.statusCode + ": Bad Request");
-        message.reply("I don't know why, but something broke.");
-        break;
-      case 404:
-        break;
-      case 429: console.log("lolapi error " + err.statusCode + ": Too many requests");
-        message.reply("there's been too many requests! Please try again in a moment.");
-        break;
-      case 500:
-      case 502:
-      case 503:
-      case 504:
-        console.log("lolapi error " + err.statusCode + ": Server unreachable");
-        message.reply("Riot's servers seem to be unreachable right now.");
-        break;
-      default:
-        console.log(err);
+    if(err.hasOwnProperty('statusCode')) {
+      switch(err.statusCode) {
+        case 400: console.log("lolapi error " + err.statusCode + ": Bad Request");
+          message.reply("I don't know why, but something broke.");
+          break;
+        case 404:
+          break;
+        case 429: console.log("lolapi error " + err.statusCode + ": Too many requests");
+          message.reply("there's been too many requests! Please try again in a moment.");
+          break;
+        case 500:
+        case 502:
+        case 503:
+        case 504:
+          console.log("lolapi error " + err.statusCode + ": Server unreachable");
+          message.reply("Riot's servers seem to be unreachable right now.");
+          break;
+        default:
+          console.log(err);
+      }
+    }
+    else {
+      console.log(err);
     }
   }
 }
--- mongodb/redirector.js
+++ mongodb/redirector.js
@@ -5,6 +5,6 @@ module.exports = {
   getDirectoryId: async function(db, guild) {
     db = await mongodb.getDb();
     var servers = await db.db("kami_db").collection("servers").find({ serverid: guild.id }).toArray();
-    return servers[0].directoryid;
+    return servers[0].directoryid + "";
   }
 };
