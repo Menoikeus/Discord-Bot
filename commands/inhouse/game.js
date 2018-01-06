43f4844946f12a7cd57a4f9d50abc7d687fe9188
Menoikeus <datonnguyen@gmail.com>
2018-01-06 16:15:35 -0500

    Some inhouse commands now don't search for commands if there is no prefix

--- commands/inhouse/game.js
+++ commands/inhouse/game.js
@@ -2,12 +2,17 @@ exports.run = (client, message, args) => {
   // if there are arguments, pass this command to the actual commands
   if(args != null && args.length > 0)
   {
-    try{
+    try {
       let commandFile = require('./game/'+args[0]+'.js');
       commandFile.run(client, message, args.slice(1));
-    }catch(err)
-    {
-      console.error(err);
+    }
+    catch(err) {
+      if(err.hasOwnProperty("code") && err.code == "MODULE_NOT_FOUND") {
+    		console.error("No command with name " + args[0]);
+      }
+      else {
+        console.log(err);
+      }
     }
     return;
   }
